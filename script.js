71040e4978db7035c2a6a119591aa6564b116657
Daniel <mishenkodaniil512@gmail.com>
2025-10-20 21:46:51 +0300

    Fixed change theme
    Fixed change theme

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ body.classList.add(currentTheme);
 
 // Переключение тем
 toggleButton.addEventListener('click', () => {
+  document.getElementById("year").textContent = new Date().getFullYear();
   if (body.classList.contains('light-theme')) {
     body.classList.replace('light-theme', 'dark-theme');
     icon.setAttribute('d', 'M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'); // Иконка луны
@@ -18,18 +19,5 @@ toggleButton.addEventListener('click', () => {
     body.classList.replace('dark-theme', 'light-theme');
     icon.setAttribute('d', 'M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'); // Иконка солнца
     localStorage.setItem('theme', 'light-theme');
-  }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Заполнение года
-  document.getElementById("year").textContent = new Date().getFullYear();
-
-  // Логика переключения темы
-  const button = document.getElementById("theme-toggle");
-  if (button) {
-    button.addEventListener("click", () => {
-      document.body.classList.toggle("dark-theme");
-    });
   }
 });
